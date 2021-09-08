0700905a5062642705953b63d86a877964163ed0
obratim <none@none>
2021-09-09 01:32:11 +0300

    configuration fix

--- GarLoader.Engine/UpdaterConfiguration.cs
+++ GarLoader.Engine/UpdaterConfiguration.cs
@@ -10,7 +10,6 @@ namespace GarLoader.Engine
         public string ServiceUri { get; set; }
         public int? RegionsCount { get; set; }
         public string ArchivesDirectory { get; set; }
-        public string RegionNumber { get; set; }
         public TimeSpan? ArchiveDownloadTimeout { get; set; }
         public TimeSpan? DbExecuteTimeout { get; set; }
         
@@ -27,9 +26,8 @@ namespace GarLoader.Engine
                     ConnectionString = this.ConnectionString ?? basicConfiguration.ConnectionString,
                     ServiceUri = this.ServiceUri ?? basicConfiguration.ServiceUri ?? "https://fias.nalog.ru/WebServices/Public/GetAllDownloadFileInfo",
                     ArchivesDirectory = this.ArchivesDirectory ?? basicConfiguration.ArchivesDirectory ?? System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "./archives/"),
-                    RegionNumber = this.RegionNumber ?? basicConfiguration.RegionNumber,
-                    ArchiveDownloadTimeout = this.ArchiveDownloadTimeout ?? basicConfiguration.ArchiveDownloadTimeout ?? TimeSpan.FromHours(12),
-                    DbExecuteTimeout = this.DbExecuteTimeout ?? basicConfiguration.DbExecuteTimeout ?? TimeSpan.FromSeconds(900),
+                    ArchiveDownloadTimeout = this.ArchiveDownloadTimeout ?? basicConfiguration.ArchiveDownloadTimeoutValue,
+                    DbExecuteTimeout = this.DbExecuteTimeout ?? basicConfiguration.DbExecuteTimeoutValue,
                     GarFullPath = this.GarFullPath ?? basicConfiguration.GarFullPath,
                     RegionsCount = this.RegionsCount ?? basicConfiguration.RegionsCountValue,
                 };
